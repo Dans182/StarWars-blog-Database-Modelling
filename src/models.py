f9cf3ea17a662ab2a057daf44ef782bbf79ab7ce
Dans182 <dgaiteiro@gmail.com>
2022-04-28 19:53:37 +0000

    v2

--- src/models.py
+++ src/models.py
@@ -10,60 +10,57 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer(250), primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     username = Column(String(250), nullable=False, unique=True)
     email = Column(String(250), nullable=False, unique=True)
-    password = Column(String(250)
+    password = Column(String(250))
 
 class Favorite(Base):
     __tablename__ = 'favorite'
- 
+    
     id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, Foreign_key("character_id")
-    character = relationship(character)
-    planet_id = Column(Integer, Foreign_key("planet_id")
-    planet = relationship(planet)
-    vehicle_id = Column(Integer, Foreign_key("vehicle_id")
-    vehicle = relationship(vehicle)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
 
 class Character(Base):
     __tablename__ = 'character'
 
     id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, Foreign_key("character_id")
-    character = relationship(character)
-    planet_id = Column(Integer, Foreign_key("planet_id")
-    planet = relationship(planet)
-    vehicle_id = Column(Integer, Foreign_key("vehicle_id")
-    vehicle = relationship(vehicle)
-    
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)    
+    favorite_id = Column(Integer, ForeignKey("favorite.id"))
+    Favorite = relationship(Favorite)
+
 class Planet(Base):
     __tablename__ = 'planet'
 
     id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, Foreign_key("character_id")
-    character = relationship(character)
-    planet_id = Column(Integer, Foreign_key("planet_id")
-    planet = relationship(planet)
-    vehicle_id = Column(Integer, Foreign_key("vehicle_id")
-    vehicle = relationship(vehicle)
-
+    name = Column(String(250), nullable=False)
+    gravity = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    favorite_id = Column(Integer, ForeignKey("favorite.id"))
+    Favorite = relationship(Favorite)
+    
 class Vehicle(Base):
     __tablename__ = 'vehicle'
 
     id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, Foreign_key("character_id")
-    character = relationship(character)
-    planet_id = Column(Integer, Foreign_key("planet_id")
-    planet = relationship(planet)
-    vehicle_id = Column(Integer, Foreign_key("vehicle_id")
-    vehicle = relationship(vehicle)    
+    name = Column(String(250), nullable=False)
+    model = Column(String(250), nullable=False)
+    capacity = Column(Integer, nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+    favorite_id = Column(Integer, ForeignKey("favorite.id"))
+    Favorite = relationship(Favorite)
 
-    def to_dict(self):
-        return {}
+def to_dict(self):
+    return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
